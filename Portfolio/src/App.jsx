1a34c1a74b6db6022038a35690ec8731b3b40ac3
akmanil <maharanaanil424@gmail.com>
2025-05-17 09:47:13 +0530

    Adding services component to the App

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Componets/Header"
 import About from "./Componets/About"
+import Services from "./Componets/Services";
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <About />
+      <Services />
     </>
   );
 }
