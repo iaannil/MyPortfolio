6f0dd5db2ed0a6d34d4383b7d21c4fc41ba8ccf4
akmanil <maharanaanil424@gmail.com>
2025-05-17 09:36:29 +0530

    Adding About section to App

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Componets/Header"
+import About from "./Componets/About"
 import './App.css'
 
 function App() {
   return (
     <>
       <Header />
+      <About />
     </>
   );
 }
